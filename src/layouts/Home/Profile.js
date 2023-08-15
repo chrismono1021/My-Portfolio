8b87ce53ec1165741d3392a4931ddd2e5d0eca92
chrismono1021 <chrismono1021@gmail.com>
2023-08-15 14:06:10 -0400

    update content

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -21,11 +21,11 @@ const ProfileText = ({ visible, titleId }) => (
       <DecoderText text="Hi there" start={visible} delay={500} />
     </Heading>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      I’m Hamish, currently I live in Sydney working as a senior product designer at{' '}
-      <Link href="https://www.qwilr.com">Qwilr</Link>. My projects include UX design, UI
-      animations, and icon illustration. Being comfortable with code allows me to rapidly
-      prototype and validate experiences. If you’re interested in the tools and software I
-      use check out my <Link href="/uses">uses page</Link>.
+      I’m Chrismono Himawan, currently I live in Sydney working as a senior product
+      designer at <Link href="https://www.qwilr.com">Qwilr</Link>. My projects include UX
+      design, UI animations, and icon illustration. Being comfortable with code allows me
+      to rapidly prototype and validate experiences. If you’re interested in the tools and
+      software I use check out my <Link href="/uses">uses page</Link>.
     </Text>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
       In my spare time I like to practice Brazilian Jiu Jitsu, play video games, and{' '}
